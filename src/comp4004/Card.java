15cb8fc492681d4e0d7db5308cc97a08f100167d
Eric <eric.nobody@gmail.com>
2015-09-19 18:21:08 -0400

    Test Passed
    Card class instantiated.
    *NOTE* org.apache.commons.lang required...

--- src/comp4004/Card.java
+++ src/comp4004/Card.java
@@ -1,19 +1,35 @@
 package comp4004;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class Card {
 
+	private static final ArrayList<String> numbers = new ArrayList<String>(
+			Arrays.asList(null, null, "Two", "Three", "Four", "Five", "Six",
+					"Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King",
+					"Ace"));
+
+	private int value;
+	private String suite;
+	
 	public Card(String string) {
-		// TODO Auto-generated constructor stub
+		String[] card = StringUtils.splitByCharacterTypeCamelCase(string);
+		if (numbers.contains(card[0])){
+			this.value = numbers.indexOf(card[0]);
+		}
+		this.suite = card[1];
+			
 	}
 
 	public Object getValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return value;
 	}
 
 	public Object getSuite() {
-		// TODO Auto-generated method stub
-		return null;
+		return suite;
 	}
 
 }
